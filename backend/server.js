25937a942ac24326854d99dd6272d82e93cb017a
harshit-506 <130093039+harshit-1245@users.noreply.github.com>
2023-12-03 01:01:49 +0530

    successfully added payment

--- backend/server.js
+++ backend/server.js
@@ -2,38 +2,34 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const cors = require('cors');
-const stripe = require('stripe')('sk_test_51O2qKzSG2lnNtXsEoZd8rcQOBmQuBdSWR6NNMpPbhS0rv4VlVJkhftu9LG28fCYRGxJeHMiZBCAb3m3KaW99q72300c2xEfgwU');
+const Razorpay = require('razorpay');
+
 
 const port = process.env.PORT || 7000;
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/api/create-checkout-session', async (req, res) => {
+
+ const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET,
+});
+
+// Endpoint to create a Razorpay order
+app.post('/api/create-razorpay-order', async (req, res) => {
   try {
-    const { products } = req.body;
+    const { amount, currency, receipt } = req.body;
 
-    const lineItems = products.map((product) => ({
-      price_data: {
-        currency: 'usd',
-        product_data: {
-          name: product.name,
-          images: [product.image],
-        },
-        unit_amount: product.price * 100,
-      },
-      quantity: product.quantity,
-    }));
+    const options = {
+      amount: amount * 100, // Razorpay accepts amount in smallest currency unit (e.g., paisa for INR)
+      currency,
+      receipt,
+    };
 
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: lineItems,
-      mode: 'payment',
-      success_url: 'http://localhost:3000/success',
-      cancel_url: 'http://localhost:3000/cancel',
-    });
+    const order = await razorpay.orders.create(options);
 
-    res.status(200).json({ sessionId: session.id }); // Sending only the session ID back to the client
+    res.status(200).json({ order }); // Sending the order details back to the client
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -1,51 +1,76 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Context } from '../context/context';
+import axios from "axios"
 import { useNavigate } from 'react-router-dom';
-import { loadStripe } from '@stripe/stripe-js';
+
 
 const Cart = () => {
   const { cart, removeFromCart, calculateSubtotal, handleProductQuantity } = useContext(Context);
   const navigate = useNavigate();
+  const [orderId,setOrderId]=useState('');
+  const [razorpayLoaded,setRazorpayLoaded]=useState(false);
+
+useEffect(()=>{
+  const loadRazorpay=async()=>{
+    const script=document.createElement('script');
+    script.src= 'http://checkout.razorpay.com/v1/checkout.js';
+    script.onload=()=>setRazorpayLoaded(script);
+  };
+  loadRazorpay();
+},[])
+
+
+  const generateReceiptId=()=>{
+    const chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result='';
+    const charactersLength=chars.length;
+    const length=10;
+
+    for(let i=0;i<length;i++){
+      result +=chars.charAt(Math.floor(Math.random()*charactersLength));
+    }
+    return result;
+  }
+  const receiptId=generateReceiptId();
 
   // Payment integration
-  const makePayment = async () => {
+  const createRazorpayOrder=async()=>{
     try {
-      const stripe = await loadStripe('pk_test_51O2qKzSG2lnNtXsE9HCOr5NmFw1c8yXNU5S9qnwywTa2VxUKyRw0PDPikDmOQdfL0AQu9I9RfxM7rL2th5sCRO6Q00eXATR9aQ');
-      const body = {
-        products: cart,
-      };
-      const headers = {
-        'Content-Type': 'application/json',
-      };
-      const response = await fetch('http://localhost:7000/api/create-checkout-session', {
-      method: 'POST',
-      headers: headers,
-      body: JSON.stringify(body),
+      const response=await axios.post('http://localhost:7000/api/create-razorpay-order',{
+        amount:calculateSubtotal(),
+        currency:'USD',
+        reciept:receiptId,
       });
+      const {order}=response.data;
+      setOrderId(order.id);
 
-      const responseData = await response.json();
-      const { sessionId } = responseData;  // Extract sessionId from the response
-      
-  
-      if (!sessionId) {
-        console.error('Session ID is missing from the server response');
-        // Handle the missing sessionId error appropriately
-        return;
-      }
-  
-      const result = await stripe.redirectToCheckout({
-        sessionId: sessionId,
-      });
-  
-      if (result.error) {
-        console.error(result.error);
-        // Handle the error, display it to the user, etc.
-      }
+      const options = {
+        key:'rzp_test_44pVLYU6Z50n9V',
+        amount:order.amount,
+        currency:order.currency,
+        name:'Shopping mania',
+        description:'SuccessFully done',
+        order_id:order.id,
+        handler:function(response){
+          console.log(response)
+        },
+        prefill: {
+          name: 'Customer Name',
+          email: 'customer@example.com',
+          contact: '9999999999',
+        },
+        theme: {
+          color: '#F37254',
+        },
+      };
+
+      const razorpay=new window.Razorpay(options);
+      razorpay.open();
     } catch (error) {
-      console.error('Error processing payment:', error);
-      // Handle unexpected errors during payment processing
+      console.error('Error creating Razorpay order:', error);
     }
-  };
+  }
+ 
   
 
   return (
@@ -78,7 +103,8 @@ const Cart = () => {
       <div className="checkout">
         <p>Subtotal: ${calculateSubtotal()}</p>
         <button onClick={() => navigate('/')}>Continue Shopping</button>
-        <button onClick={makePayment}>Checkout</button>
+        <button onClick={createRazorpayOrder}>Checkout</button>
+        {orderId && <p>OrderID:{orderId}</p>}
       </div>
     </div>
   );
